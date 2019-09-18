0345a712fca8d5c1288ce56b0b0c41859349d6ad
Adam Grise <adamwgrise@gmail.com>
2019-09-17 19:30:19 -0500

    Syntax updates

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,31 +1,27 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // Load User model
-const Item = require("../../models/Item");
+const Item = require('../../models/Item')
 
 // Add Grocery Item
-router.post("/add", (req, res) => {
-
-    const newItem = new Item({
-        name: req.body.name,
-        quantity: req.body.quantity,
-        currentlyOut: req.body.currentlyOut
-    });
-    newItem.save();
-    res.send();
+router.post('/add', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name,
+    quantity: req.body.quantity,
+    currentlyOut: req.body.currentlyOut
+  })
+  newItem.save()
+  res.send()
 })
 
 // Get grocery item
 
-router.post("/itemlist", (req, res) => {
-    
-    Item.find({}, () => {
-      const itemList = {};
-  
-      res.send(itemList);  
-    });
-  });
+router.post('/itemlist', (req, res) => {
+  Item.find({}, () => {
+    const itemList = {}
+    res.send(itemList)
+  })
+})
 
-  
-module.exports = router;
+module.exports = router
