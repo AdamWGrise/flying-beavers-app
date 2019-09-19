cc680aa819d53afe14f528f1cd7c9c3cd2e4b1df
Jebbla <jlp2722@gmail.com>
2019-09-19 18:49:14 -0500

    change

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="d-flex justify-content-center">
+      - The Flying Beavers| Roisin, Tara, Allison, Kiya, Adam, Jessica -
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Nav extends Component {
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top py-4">
+                <div className="container">
+                    <a className="navbar-brand" href="/">Flying Beavers</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+                        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/" value="Home"> Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/lists" value="Shopping">Shopping</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/lists" value="Register">Register/Login</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Nav;
