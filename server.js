08badfe61cee5e90e2ac823fd6070779b78a66d8
Adam Grise <adamwgrise@gmail.com>
2019-09-19 19:26:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,6 @@ app.use('/api/users', users);
 app.use("/api/shopItems", shopItems);
 app.use(routes);
 
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
