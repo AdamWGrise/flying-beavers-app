c56d5d3fe14c56e2db26a789294956dd8878b223
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-09-20 22:42:19 -0500

    Add item button for Lists component

--- client/src/pages/Lists.js
+++ client/src/pages/Lists.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Footer from '../components/Footer';
 import ContentPanel from '../components/ContentPanel';
 import ListClick from '../components/ListClick';
+import { Input, TextArea, FormBtn } from "../components/Form";
+
 import Jumbotron from '../components/Jumbotron';
 import API from "../utils/API";
 import DeleteBtn from "../components/DeleteBtn";
@@ -10,33 +12,26 @@ import { List, ListItem } from "../components/List";
 class Lists extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            activeListId: 0,
+            activePageTitle: 'Lists',
+            // Below: Adding items for db parts of state
+            shopItems: [],
+            shoppingList: "",
+            itemName: "",
+            category: "",
+            quantity: "",
+            quantityUnits: "",
+            newItem: ""
+        };
         this.handleListClick = this.handleListClick.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
-    state = {
-        activeListId: 0,
-        // testLists is providing test data until we are hooked up to the database.
-        testLists: [
-            { title: 'HyVee', items: ['Apples', 'Bananas', 'Blueberries', 'Pineapples', 'Kiwi', 'Plums'] },
-            { title: 'Costco', items: ['Tissues', 'Cereal', 'Dog Food', 'Toothpaste', 'Vitamins'] },
-            { title: 'Another List', items: ['Hey', 'Hey', 'hey'] }
-        ],
-        activePageTitle: 'Lists',
-        // Below: Adding items for db parts of state
-        shopItems: [],
-        shoppingList: "",
-        itemName: "",
-        category: "",
-        quantity: "",
-        quantityUnits: ""
-    };
 
-    handleListClick = (event) => {
-        console.log('CLICKY');
-        event.preventDefault();
-        this.setState({
-            activeListId: event.target.value
-        });
-    }
+
+
+
 
     // ,.-~*'`'*~-.,
     // ,.-~*'`'*~-.,
@@ -48,38 +43,50 @@ class Lists extends Component {
 
     loadShopItems = () => {
         console.log("loading shop items");
-    API.getShopItems()
-        .then(res => this.setState({ shopItems: res.data, shoppingList: "", itemName: "", category: "", quantity: "", quantityUnits: "" })
-        )
-        .catch(err => console.log(err));
+        API.getShopItems()
+            .then(res => this.setState({ shopItems: res.data, shoppingList: "", itemName: "", category: "", quantity: "", quantityUnits: "" })
+            )
+            .catch(err => console.log(err));
     };
 
     deleteShopItem = id => {
-    API.deleteShopItem(id)
-        .then(res => this.loadShopItems())
-        .catch(err => console.log(err));
+        API.deleteShopItem(id)
+            .then(res => this.loadShopItems())
+            .catch(err => console.log(err));
     };
 
+    handleListClick = (event) => {
+        console.log('CLICKY');
+        event.preventDefault();
+        this.setState({
+            activeListId: event.target.value
+        });
+    }
+
     handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-        [name]: value
-    });
+        console.log("test");
+        const { name, value } = event.target;
+        console.log(event.target);
+
+        this.setState({
+            newItem: value
+        });
     };
 
     handleFormSubmit = event => {
-    event.preventDefault();
-    if (this.state.shoppingList && this.state.itemName && this.state.quantity && this.state.quantityUnits) {
-        API.saveShopItem({
-        shoppingList: this.state.shoppingList,
-        category: this.state.category,
-        itemName: this.state.itemName,
-        quantity: this.state.quantity,
-        quantityUnits: this.state.quantityUnits,
-        })
-        .then(res => this.loadShopItems())
-        .catch(err => console.log(err));
-    }
+        console.log("clicky");
+        event.preventDefault();
+        if (this.state.shoppingList && this.state.itemName && this.state.quantity && this.state.quantityUnits) {
+            API.saveShopItem({
+                shoppingList: this.state.shoppingList,
+                category: this.state.category,
+                itemName: this.state.itemName,
+                quantity: this.state.quantity,
+                quantityUnits: this.state.quantityUnits,
+            })
+                .then(res => this.loadShopItems())
+                .catch(err => console.log(err));
+        }
     };
     // `'*~-.,.-~*'`
     // `'*~-.,.-~*'`
@@ -93,52 +100,38 @@ class Lists extends Component {
                             <Jumbotron pageName={this.state.activePageTitle} />
                         </div>
                     </div>
-                    {/* ,.-~*'`'*~-., */}
-                    {/* ,.-~*'`'*~-., */}
-                    {/* Adam 9/19 - Test area for displaying items */}
+                    <form>
+                        <Input
+                            value={this.state.newItem}
+                            onChange={this.handleInputChange}
+                            name="newItem"
+                            placeholder="Add a new list item:"
+                        />
+                        <FormBtn
+                            onClick={this.handleFormSubmit}
+                        >
+                        Add item
+                        </FormBtn>
+                    </form>
+
                     <div className='row'>
-                        <div className='col-sm-3 my-3'>
-                            <ListClick
-                                activeListId={this.state.activeListId}
-                                handleListClick={this.handleListClick}
-                                testLists={this.state.testLists}
-                            />
-                        </div>
-                        <div className='col-sm-9 my-3'>
-                        <List>
-                            {this.state.shopItems.map(shopItem => (
-                            <ListItem key={shopItem._id}>
-                                <strong>
-                                    {shopItem.itemName}, {shopItem.quantity} {shopItem.quantityUnits}
-                                </strong>
-                                <DeleteBtn onClick={() => this.deleteShopItem(shopItem._id)} />
-                            </ListItem>
-                            ))}
-                        </List>
+                        <div className='col-sm'>
+                            <List>
+                                {this.state.shopItems.map(shopItem => (
+                                    <ListItem key={shopItem._id}>
+                                        <strong>
+                                            {shopItem.itemName}, {shopItem.quantity} {shopItem.quantityUnits}
+                                        </strong>
+                                        <DeleteBtn onClick={() => this.deleteShopItem(shopItem._id)} />
+                                    </ListItem>
+                                ))}
+                            </List>
                         </div>
                     </div>
-                    {/* `'*~-.,.-~*'` */}
-                    {/* `'*~-.,.-~*'` */}
-                    <p>(Original test area below - remove before go-live)</p>
-                    <div className='row'>
-                        <div className='col-sm-3 my-3'>
-                            <ListClick
-                                activeListId={this.state.activeListId}
-                                handleListClick={this.handleListClick}
-                                testLists={this.state.testLists}
-                            />
-                        </div>
-                        <div className='col-sm-9 my-3'>
-                            <ContentPanel
-                                activeListId={this.state.activeListId}
-                                testLists={this.state.testLists}
-                            />
-                        </div>
-                    </div>
-                    <div className='row'>
-                        <div className='col-sm-12 my-3'>
-                            <Footer />
-                        </div>
+                </div>
+                <div className='row'>
+                    <div className='col-sm-12 my-3'>
+                        <Footer />
                     </div>
                 </div>
             </div>
