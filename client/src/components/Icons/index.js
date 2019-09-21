5d09342817bfa7453abf38980e365cd80117b36a
Roisin Hayes Owens <roisinhayesowens@gmail.com>
2019-09-21 14:17:42 -0500

    responsive-icons

--- client/src/components/Icons/index.js
+++ client/src/components/Icons/index.js
@@ -2,11 +2,49 @@ import React from 'react'
 import './styles.css'
 import '../../../node_modules/font-awesome/css/font-awesome.min.css'
 
-function Icons () {
-    return (
-      <div><i className="fa fa-calendar"></i></div>
-    
-    )
-  }
-  
-  export default Icons
+function Icons() {
+  return (
+    <div>
+      <div className="tooltip"><i className="fa fa-calendar fa-3x"></i>
+        <span className="tooltiptext">Tooltip text</span>
+      </div>
+      <div className="tooltip"><i className="fa fa-edit fa-3x"></i>
+        <span className="tooltiptext">dfghjk</span>
+      </div>
+    </div>
+
+
+
+
+    //     //   <div className="tooltip">
+    //     //     <div className="fa fa-calendar fa-3x">
+    //     //       <span className="tooltiptext">xcvbncfghj</span>
+
+    //     //   <div>
+    //     //     <i className="fa fa-edit fa-3x" className="tooltip">bjkfd</i>
+    //     //   </div>
+    //     // </div>
+    //     // </div>
+    //     <div>
+
+    // <div className="tooltip">
+    //   {/* <i className="fa fa-calendar fa-3x"> */} text
+    //     <span className="tooltiptext">Tooltip text</span>
+    //       {/* </i> */}
+    //       </div>
+    //       </div>
+
+
+
+
+
+
+
+
+
+
+
+  )
+}
+
+export default Icons
