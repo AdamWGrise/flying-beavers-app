20a66ab61229909c16b8eb5a652eef41ff1d42e0
Adam Grise <adamwgrise@gmail.com>
2019-09-21 23:21:19 -0500

    build changes

---
+++ client/build/precache-manifest.8a7d0a4e1d548e13d0d0f47be58c0e3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9f4e58a86cf5d55b5230b724962f373",
+    "url": "/static/media/planner.e9f4e58a.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "07627df80a88e5aa5ba6",
+    "url": "/static/js/main.db8a735a.chunk.js"
+  },
+  {
+    "revision": "09b362d13ed660c665bc",
+    "url": "/static/js/2.48227d0a.chunk.js"
+  },
+  {
+    "revision": "07627df80a88e5aa5ba6",
+    "url": "/static/css/main.87785893.chunk.css"
+  },
+  {
+    "revision": "d98bb064662abfed84409169a0f34aa3",
+    "url": "/index.html"
+  }
+];
