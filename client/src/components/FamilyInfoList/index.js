568420ac299b2e7f3c9ea0152a2bb1dfc7549305
Adam Grise <adamwgrise@gmail.com>
2019-09-28 12:45:51 -0500

    CSS cleanup - removed unused files, consolidated into the pages > styles.css file

--- client/src/components/FamilyInfoList/index.js
+++ client/src/components/FamilyInfoList/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import './styles.css'
 import FamilyInfoButton from '../FamilyInfoButton'
 
 function FamilyInfoList (props) {
--- client/src/components/ListClick/index.js
+++ client/src/components/ListClick/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import './styles.css'
 import Button from '../Button'
 
 function ListClick (props) {
