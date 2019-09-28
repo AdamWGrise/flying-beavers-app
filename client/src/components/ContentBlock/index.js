771f5a0cc8c61aac44e7567d9fc2b86810a89782
Roisin Hayes Owens <roisinhayesowens@gmail.com>
2019-09-28 14:17:55 -0500

    changes to fa icons on landing page

--- client/src/components/ContentBlock/index.js
+++ client/src/components/ContentBlock/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './styles.css'
 
-function ContentBlock (props) {
+function ContentBlock(props) {
   return (
     <div className='content-block'>
       <h1>{props.heading}</h1>
+      <h1>{props.heading2}</h1>
+      <br></br>
       <p>{props.paragraph}</p>
+
     </div>
   )
 }
--- client/src/components/Icons/index.js
+++ client/src/components/Icons/index.js
@@ -2,11 +2,20 @@ import React from 'react'
 import './styles.css'
 import '../../../node_modules/font-awesome/css/font-awesome.min.css'
 
-function Icons () {
-    return (
-      <div><i className="fa fa-calendar"></i></div>
-    
-    )
-  }
-  
-  export default Icons
+function Icons() {
+  return (
+    <div>
+      <div className="tooltip" data-placement="top"><i className="fa fa-calendar fa-3x" ></i>
+        <span className="tooltiptext">sync the family calenders</span>
+      </div>
+      <div className="tooltip" data-placement="top"><i className="fa fa-edit fa-3x"></i>
+        <span className="tooltiptext">to do list</span>
+      </div>
+      <div className="tooltip" data-placement="top"><i className="fa fa-address-card fa-3x"></i>
+        <span className="tooltiptext">family info</span>
+      </div>
+    </div>
+  )
+}
+
+export default Icons
