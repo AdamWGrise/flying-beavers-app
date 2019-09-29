80954affc16437c62388eb75514264feb1cb3041
Adam Grise <adamwgrise@gmail.com>
2019-09-28 22:48:24 -0500

    added some comments

--- client/src/components/Icons/index.js
+++ client/src/components/Icons/index.js
@@ -5,7 +5,7 @@ import '../../../node_modules/font-awesome/css/font-awesome.min.css'
 function Icons() {
     return (
     <div className='icon-set'>
-        <div className="icon" data-placement="top"><i className="fa fa-3x fa-calendar" ></i>
+        <div className="icon" data-placement="top"><i className="fa fa-3x fa-calendar"></i>
         <span className="tooltiptext">Family Calendars</span>
         </div>
         <div className="icon" data-placement="top"><i className="fa fa-3x fa-edit"></i>
