12e69b07dc7a87859d56a97b038dbb062fd712a6
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-09-30 11:10:17 -0500

    Temporary array displaying events

--- client/src/components/GCalendar/index.js
+++ client/src/components/GCalendar/index.js
@@ -6,8 +6,40 @@ import moment from "moment";
 import './styles.css';
 
 const localizer = momentLocalizer(moment);
+const now = new Date();
+const bevents = [
+    {
+        id: 0,
+        title: 'All Day Event very long title',
+        allDay: true,
+        start: new Date(2019, 6, 0),
+        end: new Date(2019, 6, 1),
+    },
+    {
+        id: 1,
+        title: 'Long Event',
+        start: new Date(2019, 3, 7),
+        end: new Date(2019, 3, 10),
+    },
+    {
+        id: 2,
+        title: 'Right now Time Event',
+        start: now,
+        end: now,
+    },
+]
+
+function convertEvents(events) {
+    events = JSON.parse(JSON.stringify(events)); // make a deep copy
+    events.forEach(ev => {
+        ev.title = ev.summary;
+    });
+    console.log("hi", events);
+    return events;
+}
 
 function GCalendar(props) {
+    let events = convertEvents(props.events);
     if (props.sign) {
         if (props.events.length > 0) {
             return (
@@ -16,7 +48,7 @@ function GCalendar(props) {
                         localizer={localizer}
                         defaultDate={new Date()}
                         defaultView="month"
-                        events={props.events}
+                        events={bevents}
                         style={{ height: "100vh" }}
                     />
                     <div>
